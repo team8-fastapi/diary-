d2cf70b09d02b7a41981bc9667cd3b4558b3f816
aidoneus9 <ribosome927@gmail.com>
2025-06-09 18:32:57 +0900

    ✨ User.py 수정
    User class에서 TimestampMixin 상속으로 수정

--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from app.database import Base
 
 
-class User(Base):
+class User(Base, TimestampMixin):
     __tablename__ = "users"
 
     user_id = Column(Integer, primary_key=True, autoincrement=True)
