28e73bc2530a77f8171416e42d860cc24891474a
eunjaegu <eunjaegu@users.noreply.github.com>
2025-06-09 18:32:49 +0900

    add models_diary

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,19 @@
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, Integer, DateTime, Text, Enum, ForeignKey
 from datetime import datetime
 from app.database import Base
 
+class TimestampMixin:
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-class User(Base):
-    __tablename__ = "users"
+class Diary(Base, TimestampMixin):
+    __tablename__ = "diary"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(16))
-    password = Column(String(60))
-    created_at = Column(DateTime, default=datetime.now)
+    diary_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.user.id"))
+    content = Column(Text)
+    emotion_summary = Column(Text)
+    mood = Column(Enum("기쁨", "슬픔", "분노", "피곤", "짜증", "무난", name="mood_enum"))
+
+
+
