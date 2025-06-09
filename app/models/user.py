8f05f6c0ddf0fbb622f92e6e27542389a33baa6f
aidoneus9 <ribosome927@gmail.com>
2025-06-09 17:51:32 +0900

    ✨ User 테이블 구상
    app/models/user.py User 테이블 구상

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime, Boolean
 from datetime import datetime
 from app.database import Base
 
@@ -6,7 +6,14 @@ from app.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(16))
-    password = Column(String(60))
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    name = Column(String(20), nullable=False)
+    phone_number = Column(String(15), nullable=True)
+    last_login = Column(DateTime)
+    is_staff = Column(Boolean, default=False)
+    is_admin = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
