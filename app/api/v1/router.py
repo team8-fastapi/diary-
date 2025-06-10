edc80b3874ca47bfb27421c30b86ff2460996db6
eunjaegu <eunjaegu@users.noreply.github.com>
2025-06-10 19:06:40 +0900

    update checks.yml

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
-from app.features.auth import auth_router
 
 router = APIRouter()
 
-router.include_router(auth_router, prefix="/auth", tags=["auth"])
+router.include_router(router, prefix="/auth", tags=["auth"])
